85a3b66f4cd539d36375c777b64858ef346959ee
twunderlich-tc <90630315+twunderlich-tc@users.noreply.github.com>
2021-11-16 08:49:34 -0800

    Update emptyfile.sh

--- emptyfile.sh
+++ emptyfile.sh
@@ -1,4 +1,4 @@
 curl -s \
     -H "Accept: application/vnd.github.v3+json" \
     --header "authorization: Bearer $GITHUB_TOKEN" \
-    --url "https://github.com/twunderlich-tc/need-test/pull/7")
+    --url "https://github.com/twunderlich-tc/need-test/pull/7"
